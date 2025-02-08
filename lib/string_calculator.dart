25712ed621abd3da2209b3e48178d5a36d1fa2f2
Nikhil <nikhil.d@mantralabsglobal.com>
2025-02-08 17:15:37 +0530

    negative number testtcase bug fixes

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -7,20 +7,20 @@ class StringCalculator {
     if(numbers.startsWith('//')){
       int delimiterIndex =numbers.indexOf('\n');
       delimiter= numbers.substring(2,delimiterIndex);
-      numbers=numbers.substring(delimiterIndex+1);
+      numbers=numbers.substring(delimiterIndex + 1);
     }
-    List<String> nums =numbers.split(RegExp('[$delimiter\n]'));
+    List<String> nums = numbers.split(RegExp('[$delimiter\n]'));
     int sum = 0 ;
     List<String>negativeValue =[];
     for(String num in nums){
       int n =int.parse(num);
-      if(n<0){
+      if(n < 0){
         negativeValue.add(num);
       }
       sum += n;
     }
     if(negativeValue.isNotEmpty){
-throw ArgumentError('Negative numbers nit aalowed:${negativeValue.join(', ')}');
+        throw ArgumentError("negative numbers not allowed: ${negativeValue.join(', ')}");
     }
     return sum;
   }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,30 +1,36 @@
 import 'package:string_calculator_tdd/string_calculator.dart';
 import 'package:test/test.dart';
 
-void main(){
-  test('Empty string should return 0',(){
-    expect(StringCalculator.add(''),equals(0));
+void main() {
+  test('Empty string should return 0', () {
+    expect(StringCalculator.add(''), equals(0));
   });
-  test('single number should return the number itself',(){
-    expect(StringCalculator.add('1'),equals(1));
+  test('single number should return the number itself', () {
+    expect(StringCalculator.add('1'), equals(1));
   });
-  test('two comma-separated numbers should retun their sum', (){
-    expect(StringCalculator.add('1,5'),equals(6));
+  test('two comma-separated numbers should retun their sum', () {
+    expect(StringCalculator.add('1,5'), equals(6));
   });
 
-  test ('Multiple comma-seprated numbers should return their sum',(){
-
-    expect(StringCalculator.add('1,2,3,4,5'),equals(15));
+  test('Multiple comma-seprated numbers should return their sum', () {
+    expect(StringCalculator.add('1,2,3,4,5'), equals(15));
   });
-  test('Number separetd by new lines or commas should return their sum',(){
-    expect(StringCalculator.add('4\n3,1'),equals(8));
+  test('Number separetd by new lines or commas should return their sum', () {
+    expect(StringCalculator.add('4\n3,1'), equals(8));
   });
-  test("custoem delimiter should be supported",(){
+  test("custoem delimiter should be supported", () {
     expect(StringCalculator.add('//;\n1;2'), equals(3));
   });
-  test('Negative numbers should throw an exception',(){
-    expect(()=> StringCalculator.add('1,-2,3,-4'),throwsA(predicate((e)=>
-    e is ArgumentError && e.message=="negative numbers not allowed: -2,-4"
-    )));
-  });
+  test('Negative numbers should throw an exception with the correct message', () {
+    expect(
+        () => StringCalculator.add('1,-2,3,-4'),
+        throwsA(
+          isA<ArgumentError>().having(
+          (error) => error.message,
+          'message',
+          contains('negative numbers not allowed: -2, -4'),
+          ),
+          ),
+          );
+  }); 
 }
