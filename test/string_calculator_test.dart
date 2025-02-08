d6b09e6732802869f09559f2eabe8b4eb46f67e5
Nikhil <nikhil.d@mantralabsglobal.com>
2025-02-08 15:29:22 +0530

    second test handle single number

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -5,4 +5,7 @@ void main(){
   test('Empty string should return 0',(){
     expect(StringCalculator.add(''),equals(0));
   });
+  test('single number should return the number itself',(){
+    expect(StringCalculator.add('1'),equals(1));
+  });
 }
